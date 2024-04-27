a53395293a9e799e79064afabfd3f8a2adeecdd2
Scott Wu <sw@scottwu.ca>
2024-04-28 03:20:42 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Calendar } from '$lib/components/ui/calendar/index.js';
+	import type { DateValue } from '@internationalized/date';
+
+	let value: DateValue[] = [];
+</script>
+
+<Calendar multiple bind:value />
